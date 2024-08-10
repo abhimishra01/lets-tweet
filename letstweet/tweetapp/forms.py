5612c00559c3cbab6f2a1fe50d16518e3f562e29
Abhishek Mishra <abhisk.mhra@gmail.com>
2024-08-10 10:37:16 +0530

    feat: add UserRegistrationForm

--- letstweet/tweetapp/forms.py
+++ letstweet/tweetapp/forms.py
@@ -1,8 +1,18 @@
 from django import forms
 from .models import Tweet
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class TweetForm(forms.ModelForm):
     class Meta:
         model = Tweet
-        fields = ['text', 'photo']
+        fields = ['text', 'photo'] # array when using custom model form
+        
+        
+class UserRegistrationForm(UserCreationForm):
+    emailail = forms.EmailField(max_length=254, help_text='Required. Avalid email address.')
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2') # tuple when using built in model form
+        
+        
