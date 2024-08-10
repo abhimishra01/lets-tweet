81e96313a40f99b2a7bfc5dcd6232b0833848d0c
Abhishek Mishra <abhisk.mhra@gmail.com>
2024-08-10 09:25:25 +0530

    feat: basic view for tweet/ route

--- letstweet/letstweet/settings.py
+++ letstweet/letstweet/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "tweetapp"
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'letstweet.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], # now we can keep templates inside all apps or in base project (letstweet) and it will be fetched 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
