0cf47825596d5b2b50dc60874fc8fb553895a171
Abhishek Mishra <abhisk.mhra@gmail.com>
2024-08-10 10:33:31 +0530

    wip: protected routes in views

--- letstweet/tweetapp/views.py
+++ letstweet/tweetapp/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Tweet
 from .forms import TweetForm
 from django.shortcuts import get_object_or_404
-
-# Create your views here.
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request, 'index.html')
@@ -12,6 +11,7 @@ def tweet_list(request):
     tweets = Tweet.objects.all().order_by('-created_at')
     return render(request, 'tweet_list.html', {'tweets': tweets})
 
+@login_required
 def tweet_create(request):
     if request.method == 'POST':
         form = TweetForm(request.POST, request.FILES)
@@ -24,6 +24,7 @@ def tweet_create(request):
         form = TweetForm()
         return render(request, 'tweet_form.html', {'form': form})
 
+@login_required
 def tweet_edit(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id, user = request.user)
     if request.method == 'POST':
@@ -37,8 +38,8 @@ def tweet_edit(request, tweet_id):
     else:
         form = TweetForm(instance=tweet)
         return render(request, 'tweet_form.html', {'form': form})
-    
-    
+ 
+@login_required    
 def tweet_delete(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id, user = request.user)
     if request.method == 'POST':
